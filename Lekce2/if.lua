002d1e5cbe2e58772f1c2aa35bd60c6846278ad2
DavidRajchman1 <>
2022-03-01 21:03:49 +0000

    Initial commit

---
+++ Lekce2/if.lua
@@ -0,0 +1,8 @@
+penize = io.read("*number")
+  if penize > 5000 then
+  print("super tak si to jdu koupit")
+elseif penize > 4500 then
+  print("muzu si to koupit, ale budu muset obedvat rohliky")
+else
+  print("skoda, musim jeste setrit")
+end
---
+++ main.lua
@@ -0,0 +1,59 @@
+--[[
+
+Toto je uvodni main soubor tohoto repozitare, kde najdes vyresenne vsechny priklady na roblox krouzek
+
+vice zde: https://docs.google.com/document/d/1y3bI2slQ0jeyFns2OYpqdkWA730Sads1HnWpMu0PXx4/edit?usp=sharing
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  ]]--
+
+print("klikni na /\\  SHELL")
+print("          |")
+print("          |")
+
+print()
+print("Nepouzivej zelene tlacitko k spusteni kodu")
+  
+print("vyuzij shell a prikaz: lua 'nazev_tveho_souboru.lua'")
+print()
+print("vice zde: https://docs.google.com/document/d/1y3bI2slQ0jeyFns2OYpqdkWA730Sads1HnWpMu0PXx4/edit?usp=sharing")
+
+if
